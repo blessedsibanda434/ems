a5b5fafb7327091f46fae3a251436cc3e8394fac
Blessed Sibanda <blessedsibanda434@gmail.com>
2019-01-09 20:16:33 +0200

    Added a post_save signal to automatically create a Profile

--- employee/models.py
+++ employee/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,3 +13,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return "{0} {1}".format(self.user.first_name, self.user.last_name)
+
+
+@receiver(post_save, sender=User)
+def user_is_created(sender, instance, created, **kwargs):
+    print('signalllllll')
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
