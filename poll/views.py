90b88f1364c0b728e2d0aba67d314b1ca4552cfa
Blessed Sibanda <blessedsibanda434@gmail.com>
2019-01-09 21:22:24 +0200

    Added detail page

--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from poll.models import Question
 
 def index(request):
     context = {}
+    questions = Question.objects.all()
     context['title'] = 'polls'
+    context['questions'] = questions
     return render(request, 'polls/index.html', context)
+
+
+def details(request, id=None):
+    question = get_object_or_404(Question, id=id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/details.html', context)
