bea59fa2472b4e70bac234198f2bbe69e71b2fcf
Blessed Sibanda <blessedsibanda434@gmail.com>
2019-01-10 09:33:22 +0200

    employee edit

--- employee/urls.py
+++ employee/urls.py
@@ -4,7 +4,7 @@ from employee.views import *
 urlpatterns = [
     path('', employee_list, name='employee_list'),
     path('<int:id>/details/', employee_details, name='employee_details'),
-    #path('<int:id>/edit', employee_edit, name='employee_edit'),
+    path('<int:id>/edit/', employee_edit, name='employee_edit'),
     path('add/', employee_add, name='employee_add'),
     #path('<int:id>/delete/', employee_delete, name="employee_delete")
 ]
--- employee/views.py
+++ employee/views.py
@@ -27,3 +27,16 @@ def employee_add(request):
     else:
         user_form = UserForm()
         return render(request, 'employee/add.html', {'user_form':user_form})
+
+def employee_edit(request, id=None):
+    user = get_object_or_404(User, id=id)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=user)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect(reverse('employee_list'))
+        else:
+            return render(request, 'employee/edit.html', {'user_form':user_form})
+    else:
+        user_form = UserForm(instance=user)
+        return render(request, 'employee/edit.html', {'user_form':user_form})
